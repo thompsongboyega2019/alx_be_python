4c43341b62f6a0228645ecec27d73934dfb15a14
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-01 20:07:19 +0100

    Delete and remove

--- python_introduction/future_age_calculator.py
+++
@@ -1,5 +0,0 @@
-getAge = int(input('How old are you?'))
-currentYear = 2023
-targetYear = 2050
-estimatedAge = (targetYear - currentYear) + getAge
-print("In 2050, you will be", estimatedAge, "years old.")
--- python_introduction/hours_to_seconds.py
+++
@@ -1,3 +0,0 @@
-hours = 2
-seconds = 3600 * hours
-print(hours, "hours(s) is", seconds, "seconds.")
