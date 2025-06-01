2f10b4ac9d3a6b4851ceb5c9f87fbf5b4aad11c8
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-01 20:10:13 +0100

    All modifications made

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+getAge = int(input('How old are you?'))
+currentYear = 2023
+targetYear = 2050
+estimatedAge = (targetYear - currentYear) + getAge
+print("In 2050, you will be", estimatedAge, "years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+seconds = 3600 * hours
+print(hours, "hours(s) is", seconds, "seconds.")
