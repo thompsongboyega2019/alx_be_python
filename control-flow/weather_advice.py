b71aaac3f0224e157e1915fa973f59cf56d9b5bd
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-01 20:27:20 +0100

    Updated

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,6 +4,6 @@ if weather == "sunny":
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-    print("Make sure to wear a warm coat and a scarf")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("I don't have recommendations for this weather.")
