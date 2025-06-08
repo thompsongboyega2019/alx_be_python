9037e1f2155ccc527b6b3359ec645ee942b34476
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-08 22:06:40 +0100

    Added Explore Date File

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time:", formatted_date)
+    return current_date
+
+
+def calculate_future_date(current_date):
+    try:
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
+        future_date = current_date + timedelta(days=days_to_add)
+        print("Future date:", future_date.strftime("%Y-%m-%d"))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+
+def main():
+    current_date = display_current_datetime()
+    calculate_future_date(current_date)
+
+
+if __name__ == "__main__":
+    main()
