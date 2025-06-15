5f828eb680847e719916e693eeb42db85b6c10ab
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-15 19:03:03 +0100

    Created the Programming Paradigm file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+    # Function for deposit
+    def deposit(self, amount):
+            self.account_balance += amount
+    # Function for withdraw
+    def withdraw(self, amount):
+            if self.account_balance > amount:
+                self.account_balance -= amount
+                return True
+            else:
+                return False
+    # Function to display account balance
+    def display_balance(self):
+            print(f"Current account balance = {self.account_balance}")
