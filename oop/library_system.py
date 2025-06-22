c2ccc2819680838c9f9136cb7b3f490308cba4ef
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-22 19:52:20 +0100

    Updated

---
+++ oop/library_system.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self, title:str, author:str):
+        self.title = title
+        self.author = author
+        pass
+
+# EBook subclass
+class EBook(Book):
+    def __init__(self, title, author, file_size:int):
+        super().__init__(title, author)
+        self.file_size = file_size
+        pass
+
+# PrintBook subclass
+class PrintBook(EBook):
+    def __init__(self, title, author, page_count:int):
+        super().__init__(title, author)
+        self.page_count = page_count
+        pass
+
+# Library Class
+class Library:
+    def __init__(self, books:list):
+        self.books = books
+
+    # Add book method
+    def add_book(self, book):
+        self.books.append(book)
+
+    # List books
+    def list_books(self):
+        for book in self.books:
